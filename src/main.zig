221b4775aa368993fd2eb3fdca76c117ca3a01ee
lucasbclarke <lucasbaileyclarke@outlook.com>
2025-04-27 12:13:27 +1000

    trying to implement reset functionality once the objective is reached, the objective can now be reached though

--- src/main.zig
+++ src/main.zig
@@ -6,84 +6,91 @@ const windowW: u16 = 1440;
 const windowH: u16 = 900;
 
 const player = enum {
-   var recPos = rl.Vector2.init(650, 900);
-   const recSize = rl.Vector2.init(100, 100);
-   var recC: rl.Color = .red;
+    var recPos = rl.Vector2.init(650, 900);
+    const recSize = rl.Vector2.init(100, 100);
+    var recC: rl.Color = .red;
 };
 
 const border = enum {
-   var recPos = rl.Vector2.init(0, 0);
-   const recSize = rl.Vector2.init(windowW, windowH);
-   const recC: rl.Color = .blue;
+    var recPos = rl.Vector2.init(0, 0);
+    const recSize = rl.Vector2.init(windowW, windowH);
+    const recC: rl.Color = .blue;
 };
 
 const objective = enum {
-   var recPos = rl.Vector2.init(0, 450);
-   const recSize = rl.Vector2.init(50, 200);
-   const recC: rl.Color = .pink;
+    var recPos = rl.Vector2.init(0, 450);
+    const recSize = rl.Vector2.init(50, 200);
+    const recC: rl.Color = .pink;
 };
 
 fn movementofplayer() void {
-   if (rl.isKeyDown(.j) != true and rl.isKeyDown(.k) != true) {
-      player.recPos.y += 10;
-   }
+    if (rl.isKeyDown(.j) != true and rl.isKeyDown(.k) != true) {
+        player.recPos.y += 10;
+    }
 
+    if (player.recPos.y < 1) {
+        player.recPos.y = 1;
+    } else {
+        if (rl.isKeyDown(.j)) {
+            player.recPos.y += 10;
+        }
+    }
 
-   if (player.recPos.y < 1 ) {
-      player.recPos.y = 1;
-   } else {
-      if (rl.isKeyDown(.j)) {   
-         player.recPos.y += 10;
-      }
-   }
+    if (player.recPos.y > windowH - player.recSize.y) {
+        player.recPos.y = windowH - player.recSize.y;
+    } else if (rl.isKeyDown(.k)) {
+        player.recPos.y -= 10;
+    }
 
-   if (player.recPos.y > windowH - player.recSize.y) {
-      player.recPos.y = windowH - player.recSize.y;
-   } else if (rl.isKeyDown(.k)) {   
-         player.recPos.y -= 10;
-   } 
+    if (player.recPos.x > (border.recSize.x - 100)) {
+        player.recPos.x = player.recPos.x;
+    } else { 
+        if (rl.isKeyDown(.l)) {
+            player.recPos.x += 10;
+        }
+    }
 
-   if (player.recPos.x > (border.recSize.x - 100)) {
-      player.recPos.x = player.recPos.x;
-   } else {
-      if (rl.isKeyDown(.l)) {
-         player.recPos.x += 10;
-      }                         
-   }
-
-   if (player.recPos.x < 1) {
-      player.recPos.x = player.recPos.x;
-   } else {
-      if (rl.isKeyDown(.h)) {   
-         player.recPos.x -= 10;
-      }                         
-   }
-
+    if (player.recPos.x < 1) {
+        player.recPos.x = player.recPos.x;
+    } else {
+        if (rl.isKeyDown(.h)) {
+            player.recPos.x -= 10;
+        }
+    }
 }
 
-fn touchingObject() void {
-   if (player.recPos.x <= 50) {
-      if (player.recPos.y <= 650 and player.recPos.y >= 350) {
-         rl.drawText("You have reached the objective", windowW / 2, windowH / 2, 20 , .black);
-
-      }
-   }
+fn touchingObject() bool {
+    if (player.recPos.x <= 50) {
+        if (player.recPos.y <= 650 and player.recPos.y >= 350) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
 }
 
 pub fn main() !void {
-   var camera = rl.Camera2D{
-      .target = .init(border.recPos.x, border.recPos.y),
-      .offset = .init(0, 0),
-      .rotation = 0,
-      .zoom = 1,
-   };
+    var reachedObj: bool = undefined;
 
-   rl.initWindow(windowW, windowH, "Window");
-   defer rl.closeWindow();
+    var camera = rl.Camera2D{
+        .target = .init(border.recPos.x, border.recPos.y),
+        .offset = .init(0, 0),
+        .rotation = 0,
+        .zoom = 1,
+    };
+
+    rl.initWindow(windowW, windowH, "Window");
+    defer rl.closeWindow();
 
     rl.setTargetFPS(60);
 
     while (!rl.windowShouldClose()) {
+        if (touchingObject() == true) {
+            reachedObj = true;
+        }
+
         rl.beginDrawing();
         defer rl.endDrawing();
 
@@ -98,31 +105,43 @@ pub fn main() !void {
 
         rl.drawRectangleV(objective.recPos, objective.recSize, objective.recC);
 
-        touchingObject();
+        //trying to implement reset logic
+        if (reachedObj) {
+            if (rl.isKeyDown(.r)) {
+                player.recPos.x = 650;
+                player.recPos.y = 800;
+
+                camera.target.x = border.recPos.x;
+                camera.target.y = border.recPos.y;
+                movementofplayer();
+            } else {
+                rl.drawText("You have reached the objective", windowW / 4, windowH / 2, 20, .black);
+            }
+        } else {
+            movementofplayer();
+        }
+
 
         rl.drawFPS(@intFromFloat(camera.target.x + 10), @intFromFloat(camera.target.y + 10));
-      
-        movementofplayer();    
 
         if (rl.isKeyDown(.right)) {
-           camera.target.x += 5;
+            camera.target.x += 5;
         }
 
         if (rl.isKeyDown(.left)) {
-           camera.target.x -= 5;
+            camera.target.x -= 5;
         }
 
         if (rl.isKeyDown(.up)) {
-           camera.target.y -= 5;
+            camera.target.y -= 5;
         }
 
         if (rl.isKeyDown(.down)) {
-           camera.target.y += 5;
+            camera.target.y += 5;
         }
 
         if (rl.isKeyDown(.r)) {
             camera.target = .init(0, 0);
         }
-         
     }
 }
